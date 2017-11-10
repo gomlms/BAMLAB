6e4f6d2a47cf081d2f3120f8f9e4d6f626505106
gomlms <gomlms@gmail.com>
2017-11-10 18:15:06 -0500

    Big changes

--- bam/src/Menu.js
+++ bam/src/Menu.js
@@ -1,7 +1,49 @@
 import React, {Component} from 'react';
 import './Menu.css';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 class Menu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.handleChangeRadius = this.handleChangeRadius.bind(this);
+    this.handleSubmitRadius = this.handleSubmitRadius.bind(this);
+    this.handleChangeDepth = this.handleChangeDepth.bind(this);
+    this.handleSubmitDepth = this.handleSubmitDepth.bind(this);
+
+    this.state = {
+      dropdownOpen: false,
+      firstZ: "Z-30",
+      depthValue: '',
+      radiusValue: ''
+    };
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  handleChangeRadius(event) {
+    this.setState({radiusValue: event.target.value});
+  }
+
+  handleSubmitRadius(event) {
+    console.log('A new height was submitted: ' + this.state.radiusValue);
+    event.preventDefault();
+  }
+
+  handleChangeDepth(event) {
+    this.setState({depthValue: event.target.value});
+  }
+
+  handleSubmitDepth(event) {
+    console.log('A new depth was submitted: ' + this.state.depthValue);
+    event.preventDefault();
+  }
+
   render(){
     return(
       <div className='site'>
@@ -10,146 +52,148 @@ class Menu extends Component {
             <h1>Bam Lab G-Code Generator</h1>
           </div>
           <div>
-            <div>
-              <h3 className='slider'>Pattern</h3>
-              <div className="slidecontainer">
-                <input type="range" min="1" max="100" value="50" class="slider" id="myRange" />
-              </div>
-            </div>
-            <div>
-              <h3 className='slider'>Move Speed</h3>
-              <div className="slidecontainer">
-                <input type="range" min="1" max="100" value="50" class="slider" id="myRange" />
-              </div>
-            </div>
-            <div>
-              <h3 className='slider'>Flow Rate</h3>
-              <div className="slidecontainer">
-                <input type="range" min="1" max="100" value="50" class="slider" id="myRange" />
-              </div>
-            </div>
-            <div>
-              <h3 className='slider'>Pattern Scale</h3>
-              <div className="slidecontainer">
-                <input type="range" min="1" max="100" value="50" class="slider" id="myRange" />
-              </div>
-            </div>
-            <button type="button">Submit</button>
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+              <DropdownToggle caret>
+                Pattern
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem header>Header</DropdownItem>
+                <DropdownItem disabled>Action</DropdownItem>
+                <DropdownItem>Another Action</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Another Action</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+          <div>
+            <form onSubmit={this.handleSubmitDepth}>
+              <label>
+                Depth:
+                <input type="text" value={this.state.depthValue} onChange={this.handleChangeDepth} />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+          <div>
+            <form onSubmit={this.handleSubmitRadius}>
+              <label>
+                Radius:
+                <input type="text" value={this.state.radiusValue} onChange={this.handleChangeRadius} />
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
           </div>
         </div>
-        <p className='gcode'>
-        ;Homeing Needle Gcode<br />
-        ; Michael Restaino 10/10/2017<br />
-        <br />
-        Firmware Marlin<br />
-        <br />
-        M118 Cold Extrusion<br />
-        M302 S0<br />
-        G90<br />
-        M117 Calibration<br />
-        G0 Z45 F6000<br />
-        M117 Homing X and Y<br />
-        G28 X Y<br />
-        M117 Homing Z<br />
-        G0 X45  Y100<br />
-        G28 Z<br />
-        G90<br />
-        M117 Centering Needle Over Beaker<br />
-        G0  Z45<br />
-        G0 X137.50 Y67.50<br />
+        <div className='gcodeHolder'>
+          <p className='gcode'>
+          M118 Cold Extrusion<br />
+          M302 S0<br />
+          G90<br />
+          M117 Calibration<br />
+          G0 Z45 F6000<br />
+          M117 Homing X and Y<br />
+          G28 X Y<br />
+          M117 Homing Z<br />
+          G0 X45  Y100<br />
+          G28 Z<br />
+          G90<br />
+          M117 Centering Needle Over Beaker<br />
+          G0  Z45<br />
+          G0 X137.50 Y67.50<br />
 
-        G90<br />
-        M117 Centering over Beaker<br />
-        G0  Z45 ; Raise to just above Beaker Rim<br />
-        G0 X137.50 Y67.<br />
-        M117 Switch to Relative Coordinates<br />
-        G91; Relative Coordiantes<br />
+          G90<br />
+          M117 Centering over Beaker<br />
+          G0  Z45 ; Raise to just above Beaker Rim<br />
+          G0 X137.50 Y67.<br />
+          M117 Switch to Relative Coordinates<br />
+          G91; Relative Coordiantes<br />
 
 
-        M117 Center Bottom<br />
-        G1 Z-30<br />
-        G0 F4000<br />
-        G1 E50<br />
-        M400<br />
-        G4 S15<br />
-        M400<br />
-        G0 F4000<br />
-        G1 E-25<br />
-        G4 S5<br />
-        G1 Z10<br />
-<br />
-        M117 Center Top<br />
-        G0 F4000<br />
-        G1 E40<br />
-        M400<br />
-        G4 S10<br />
-        M400<br />
-        G1 E-25<br />
-        G4 S5<br />
-        G1 Z20<br />
-<br />
-<br />
-        M117 Top Right<br />
-        G0 X15 Y15<br />
-        G1 Z-20<br />
-        G0 F4000<br />
-        G1 E30<br />
-        M400<br />
-        G4 S10<br />
-        M400<br />
-        G1 E-25<br />
-        G4 S5<br />
-        G1 Z20<br />
-<br />
-<br />
-        M117 Bottom Right<br />
-        G1 Y-30<br />
-        G1 Z-20<br />
-        G0 F4000<br />
-        G1 E30<br />
-        M400<br />
-        G4 S10<br />
-        M400<br />
-        G1 E-25<br />
-        G4 S5<br />
-        G1 Z20<br />
-<br />
-<br />
-        M117 Bottom Left<br />
-        G1 X-30<br />
-        G1 Z-20<br />
-        G0 F4000<br />
-        G1 E30<br />
-        M400<br />
-        G4 S10<br />
-        M400<br />
-        G1 E-25<br />
-        G4 S5<br />
-        G1 Z20<br />
-<br />
-<br />
-        M117 Top Left<br />
-        G1 Y30<br />
-        G1 Z-20<br />
-        G0 F4000<br />
-        G1 E30<br />
-        M400<br />
-        G4 S10<br />
-        M400<br />
-        G1 E-25<br />
-        G4 S5<br />
-        G1 Z20<br />
-<br />
-<br />
+          M117 Center Bottom<br />
+          G1 {this.state.firstZ}<br />
+          G0 F4000<br />
+          G1 E50<br />
+          M400<br />
+          G4 S15<br />
+          M400<br />
+          G0 F4000<br />
+          G1 E-25<br />
+          G4 S5<br />
+          G1 Z10<br />
+  <br />
+          M117 Center Top<br />
+          G0 F4000<br />
+          G1 E40<br />
+          M400<br />
+          G4 S10<br />
+          M400<br />
+          G1 E-25<br />
+          G4 S5<br />
+          G1 Z20<br />
+  <br />
+  <br />
+          M117 Top Right<br />
+          G0 X15 Y15<br />
+          G1 Z-20<br />
+          G0 F4000<br />
+          G1 E30<br />
+          M400<br />
+          G4 S10<br />
+          M400<br />
+          G1 E-25<br />
+          G4 S5<br />
+          G1 Z20<br />
+  <br />
+  <br />
+          M117 Bottom Right<br />
+          G1 Y-30<br />
+          G1 Z-20<br />
+          G0 F4000<br />
+          G1 E30<br />
+          M400<br />
+          G4 S10<br />
+          M400<br />
+          G1 E-25<br />
+          G4 S5<br />
+          G1 Z20<br />
+  <br />
+  <br />
+          M117 Bottom Left<br />
+          G1 X-30<br />
+          G1 Z-20<br />
+          G0 F4000<br />
+          G1 E30<br />
+          M400<br />
+          G4 S10<br />
+          M400<br />
+          G1 E-25<br />
+          G4 S5<br />
+          G1 Z20<br />
+  <br />
+  <br />
+          M117 Top Left<br />
+          G1 Y30<br />
+          G1 Z-20<br />
+          G0 F4000<br />
+          G1 E30<br />
+          M400<br />
+          G4 S10<br />
+          M400<br />
+          G1 E-25<br />
+          G4 S5<br />
+          G1 Z20<br />
+  <br />
+  <br />
 
-        M117 Homing...<br />
-        G90 ; Back to Absolute Coordinates<br />
-        G0  Z45<br />
-        G0 X137.50 Y67.50<br />
-<br />
-        M117 Finished Deposition<br />
-        G4 P500
-        </p>
+          M117 Homing...<br />
+          G90 ; Back to Absolute Coordinates<br />
+          G0  Z45<br />
+          G0 X137.50 Y67.50<br />
+  <br />
+          M117 Finished Deposition<br />
+          G4 P500
+          </p>
+        </div>
       </div>
     );
   }
