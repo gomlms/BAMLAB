2d346d8d7a5182185e85bef488ee98b10f3b7082
Gustavo Guevara <guevaragus11@gmail.com>
2017-11-12 17:02:48 -0500

    Messahe

--- bam/src/SecondPoint.js
+++ bam/src/SecondPoint.js
@@ -5,15 +5,40 @@ class SecondPoint extends Component {
     super(props, context);
 
     this.state = {
-
+      radius: props.radius,
+      amountDispursed: props.amountDispursed,
+      numLayers: props.numLayers,
+      curLayer: props.curLayer,
     };
   }
 
   render(){
     return(
-
+      M117 Two point layer <br />
+      G1 X{Math.trunc(this.state.radius)}<br />
+      G1 Z{Math.trunc(-20*this.state.curLayer/this.state.numLayers)}<br />
+      G0 F4000<br />
+      G1 E{this.state.amountDispursed}<br />
+      M400<br />
+      G4 S15<br />
+      M400<br />
+      G0 F4000<br />
+      G1 E-25<br />
+      G4 S5<br />
+      G1 Z{Math.trunc(20*this.state.curLayer/this.state.numLayers)}<br />
+      G1 X{Math.trunc(-this.state.radius)}<br />
+      G1 Z{Math.trunc(-20*this.state.curLayer/this.state.numLayers)}<br />
+      G0 F4000<br />
+      G1 E{this.state.amountDispursed}<br />
+      M400<br />
+      G4 S15<br />
+      M400<br />
+      G0 F4000<br />
+      G1 E-25<br />
+      G4 S5<br />
+      G1 Z{Math.trunc(20*this.state.curLayer/this.state.numLayers)}<br />
     );
   }
 }
 
-export default FirstPoint;
+export default SecondPoint;
