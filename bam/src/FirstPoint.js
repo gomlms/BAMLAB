90ef07fcb7fdbe9a386ba7fe3d7190fe0bc0a513
Stoney Fentonmiller <stoneyfentonmiller@yahoo.com>
2017-11-12 16:44:17 -0500

    first

--- bam/src/FirstPoint.js
+++ bam/src/FirstPoint.js
@@ -5,13 +5,27 @@ class FirstPoint extends Component {
     super(props, context);
 
     this.state = {
-      
+      numLayers: props.numLayers,
+      radius: props.radius,
+      amountDispersed: props.amountDispersed,
+      curLayer: props.curLayer,
     };
   }
 
   render(){
     return(
-
+      M117 Single point layer<br />
+      G1 Z{-1 * Math.trunc((20 * curLayer)/numLayers}<br />
+      G0 F4000<br />
+      G1 E{amountDispersed}<br />
+      M400<br />
+      G4 S15<br />
+      M400<br />
+      G0 F4000<br />
+      G1 E-25<br />
+      G4 S5<br />
+      G1 Z{Math.trunc((20 * curLayer)/numLayers}<br />
+      <br />
     );
   }
 }
