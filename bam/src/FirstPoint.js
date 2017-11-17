54c145cc6dbf3964107b25ec195e246af393a04f
gomlms <gomlms@gmail.com>
2017-11-16 22:57:47 -0500

    stoney eats calapenos

--- bam/src/FirstPoint.js
+++ bam/src/FirstPoint.js
@@ -17,26 +17,20 @@ class FirstPoint extends Component {
     return(
       <p>
       M117 Single point layer<br />
-<<<<<<< HEAD
       G1 Z{-1 * Math.trunc(20 + 100 * (this.props.curLayer)/(this.props.numLayers + 1))}<br />
       G0 F4000<br />3
       G1 E{this.props.amountDispersed}<br />
-=======
       G1 Z{-1 * Math.trunc(20 * (this.state.curLayer)/(this.state.numLayers + 1))}<br />
       G0 F4000<br />
       G1 E{this.state.amountDispersed}<br />
->>>>>>> 5ec4f6b1728946b253bbeb1d5aa2f9d47e1d0b9d
       M400<br />
       G4 S15<br />
       M400<br />
       G0 F4000<br />
       G1 E-25<br />
       G4 S5<br />
-<<<<<<< HEAD
       G1 Z{Math.trunc(20 + 100 * (this.props.curLayer)/(this.props.numLayers + 1))}<br />
-=======
       G1 Z{Math.trunc(20 * (this.state.curLayer)/(this.state.numLayers + 1))}<br />
->>>>>>> 5ec4f6b1728946b253bbeb1d5aa2f9d47e1d0b9d
       <br />
       </p>
     );
