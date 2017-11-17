af71f7860b4b74f19f11c284e67eecfdfe71a29b
Stoney Fentonmiller <stoneyfentonmiller@yahoo.com>
2017-11-16 19:27:03 -0500

    new

--- bam/src/Buttons.js
+++ bam/src/Buttons.js
@@ -1,6 +1,37 @@
 import React, {Component} from 'react';
 
 class Buttons extends Component {
-  
+  constructor(props, context) {
+    super(props, context);
+
+    this.function1 = this.function1.bind(this);
+    this.function2 = this.function2.bind(this);
+    this.function3 = this.function3.bind(this);
+  }
+
+  function1() {
+    //this.props.buttonPressed(1);
+    console.log("POOP");
+  }
+
+  function2() {
+    //this.props.buttonPressed(2);
+  }
+
+  function3() {
+    //this.props.buttonPressed(3);
+  }
+
+  render() {
+    return(
+      <div>
+        <input type="button" value="One" onClick={this.function1} />
+
+        <input type="button" value="Two" onClick={this.function2} />
+
+        <input type="button" value="Three" onClick={this.function3} />
+      </div>
+    );
+  }
 }
-export default Menu;
+export default Buttons;
